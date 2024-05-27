8bf94b4a9390e8597a0b2fd6b0d82062f387cf10
Максим Котов <pismo.max.kotov@gmail.com>
2024-05-28 02:11:26 +0300

    добавил функции для винды

--- WindowsInfo.cpp
+++ WindowsInfo.cpp
@@ -3,27 +3,91 @@
 #include <utility>
 
 uint64_t WindowsInfo::getRAMTotal() {
-    return 0;
+    MEMORYSTATUSEX statex;
+    statex.dwLength = sizeof(statex);
+    GlobalMemoryStatusEx(&statex);
+    return (float)statex.ullTotalPhys;
 }
 
 uint32_t WindowsInfo::getCPUCoresNum() {
-    return 0;
+    SYSTEM_INFO sysinfo;
+    GetSystemInfo(&sysinfo);
+    return sysinfo.dwNumberOfProcessors;
 }
 
 std::string WindowsInfo::getOSVersion() {
-    return std::string();
+	if (IsWindows10OrGreater())
+	{
+		return "Windows 10";
+	}
+	if (IsWindows8Point1OrGreater())
+	{
+		return "Windows 8.1";
+	}
+	if (IsWindows8OrGreater())
+	{
+		return "Windows 8";
+	}
+	if (IsWindows7OrGreater())
+	{
+		return "Windows 7";
+	}
+	if (IsWindowsVistaOrGreater())
+	{
+		return "Windows Vista";
+	}
+	if (IsWindowsXPOrGreater())
+	{
+		return "Windows XP";
+	}
+	return "Unknown";
 }
 
 std::string WindowsInfo::getOSArch() {
-    return std::string();
+	#if _WIN32
+	#if defined(_WIN64)
+		return "x86_64";
+	#else
+		return "x86";
+	#endif
+	#elif __ANDROID__
+		return "ARM";
+	#else
+		return "unknown";
+	#endif
 }
 
 std::string WindowsInfo::getCPUModel() {
-    return std::string();
+    return GetCpuVendorString();
 }
 
 std::string WindowsInfo::getCPUArch() {
-    return std::string();
+	SYSTEM_INFO sysinfo;
+	GetSystemInfo(&sysinfo);
+	if (sysinfo.wProcessorArchitecture == 9)
+	{
+		return "x86_64";
+	}
+	else if (sysinfo.wProcessorArchitecture == 5)
+	{
+		return "ARM";
+	}
+	else if (sysinfo.wProcessorArchitecture == 12)
+	{
+		return "ARM64";
+	}
+	else if (sysinfo.wProcessorArchitecture == 6)
+	{
+		return "Intel Itanium";
+	}
+	else if (sysinfo.wProcessorArchitecture == 0)
+	{
+		return "x86";
+	}
+	else if (sysinfo.wProcessorArchitecture == 0xffff)
+	{
+		return "Unknown architecture";
+	}
 }
 
 std::string WindowsInfo::getVAModel() {
@@ -43,19 +107,76 @@ std::vector<SysInfo::NetworkAdapterInfo> WindowsInfo::getNetworkAdaptersInfo() {
 }
 
 double WindowsInfo::getSystemUptime() {
-    return 0;
+	SYSTEMTIME sysTime;
+	//Retrieves data so that we have a way to Get it to output when using the pointers
+	GetSystemTime(&sysTime);
+	return (double)(sysTime.wHour + ((double)sysTime.wMinute / 100));
 }
 
 uint64_t WindowsInfo::getRAMFree() {
-    return 0;
+    MEMORYSTATUSEX statex;
+    statex.dwLength = sizeof(statex);
+    GlobalMemoryStatusEx(&statex);
+    return (float)statex.ullAvailPhys;
 }
 
 uint64_t WindowsInfo::getRAMUsed() {
-    return 0;
+    return getRAMTotal() - getRAMFree();
 }
 
 uint32_t WindowsInfo::getTotalCPUUsage() {
-    return 0;
+	static char cpu[MAX_PATH] = { 0 };
+
+	typedef NTSTATUS(NTAPI* pfNtQuerySystemInformation) (
+		IN SYSTEM_INFORMATION_CLASS SystemInformationClass,
+		OUT PVOID SystemInformation,
+		IN ULONG SystemInformationLength,
+		OUT PULONG ReturnLength OPTIONAL
+		);
+
+	static pfNtQuerySystemInformation NtQuerySystemInformation = NULL;
+
+	if (NtQuerySystemInformation == NULL)
+	{
+
+		// Initializing a string object
+		string Str = "ntdll.dll";
+
+		// Initializing an object of wstring
+		wstring temp = wstring(Str.begin(), Str.end());
+		// Applying c_str() method on temp
+		LPCWSTR wideString = temp.c_str();
+
+		HMODULE ntDLL = ::GetModuleHandle(wideString);
+		NtQuerySystemInformation = (pfNtQuerySystemInformation)GetProcAddress(ntDLL, "NtQuerySystemInformation");
+	}
+
+	typedef struct
+		_SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION {
+		LARGE_INTEGER IdleTime;
+		LARGE_INTEGER KernelTime;
+		LARGE_INTEGER UserTime;
+		LARGE_INTEGER Reserved1[2];
+		ULONG Reserved2;
+	} SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION;
+
+
+	static SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION lastInfo = { 0 };
+	SYSTEM_PROCESSOR_PERFORMANCE_INFORMATION curInfo = { 0 };
+
+	ULONG retsize;
+
+	NtQuerySystemInformation(SystemProcessorPerformanceInformation, &lastInfo, sizeof(lastInfo), &retsize);
+	Sleep(1000);
+	NtQuerySystemInformation(SystemProcessorPerformanceInformation, &curInfo, sizeof(curInfo), &retsize);
+	double cpuUsage = -1;
+	cpuUsage = 100.0 - double(curInfo.IdleTime.QuadPart - lastInfo.IdleTime.QuadPart) /
+		double(curInfo.KernelTime.QuadPart - lastInfo.KernelTime.QuadPart + curInfo.UserTime.QuadPart - lastInfo.UserTime.QuadPart) * 100.0;
+	_itoa_s(cpuUsage, cpu, 10);
+
+	lastInfo = curInfo;
+
+	return cpuUsage;
 }
 
 std::vector<uint32_t> WindowsInfo::getCPUCoresUsage() {
@@ -63,7 +184,20 @@ std::vector<uint32_t> WindowsInfo::getCPUCoresUsage() {
 }
 
 int WindowsInfo::getCurrentTasksNum() {
-    return 0;
+	PROCESSENTRY32 Process;
+	HANDLE hProcess;
+	Process.dwSize = sizeof(PROCESSENTRY32);
+
+	hProcess = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	int countProc = 1;
+	if (Process32First(hProcess, &Process))
+	{
+		do
+		{
+			countProc++;
+		} while (Process32Next(hProcess, &Process));
+	}
+	return countProc;
 }
 
 uint64_t WindowsInfo::getDriveCurrentWrite(std::string driveName) {
@@ -83,7 +217,25 @@ uint64_t WindowsInfo::getNetworkAdapterCurrentDownload(std::string adapterName) 
 }
 
 std::string WindowsInfo::getOSFamily() {
-    return OSFamily;
+	#if __linux__
+		return "linux";
+	#elif _WIN32
+	#if defined(_WIN64)
+		return "win64";
+	#else
+		return "win32";
+	#endif
+	#elif __APPLE__
+		return "mac";
+	#elif __sun
+		return "solaris";
+	#elif __unix__
+		return "unix";
+	#elif BSD
+		return "bsd";
+	#else
+		return "unknown";
+	#endif
 }
 
 WindowsInfo::WindowsInfo(std::string OSFamily) : SysInfo(std::move(OSFamily)) {
